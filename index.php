c9067c109739e9da3aa7fac87ee3a746801a4e98
Fernando Pinto <fernandosilvapinto@outlook.com>
2018-08-14 17:11:26 -0300

    CRUD categorias criado

--- index.php
+++ index.php
@@ -296,6 +296,42 @@ $app->get("/admin/categories/:idcategory/delete", function($idcategory){
 
 });
 
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+
+		'category'=> $category->getValues()
+
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+
 
 $app->run();
 
