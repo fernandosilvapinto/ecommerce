1d0220ef8fd610b503efcebe63919cd79603eb5d
Fernando Pinto <fernandosilvapinto@outlook.com>
2018-08-27 21:31:38 -0300

    site - produtos

--- site.php
+++ site.php
@@ -2,12 +2,17 @@
 require_once("vendor/autoload.php");
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
-    
+   
+	$products = Product::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+	]);
 
 });
 
